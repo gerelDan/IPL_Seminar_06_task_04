d59fea44882553bee26b7aaf60610cb488267bcf
gerel <gerel@bk.ru>
2023-12-18 18:23:36 +0500

    Final_01

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
 //разделенных пробелами. Сформировать строку,
 //в которой слова расположены в обратном порядке.
 // В полученной строке слова должны быть также разделены пробелами.
+string str = "Для реализации автозапуска в Linux написано уже"+
+            " немало и на разных языках но приходится искать"+
+            " потому постарался свести большую часть тут";
+Console.WriteLine(str);
+string ReversString(string str)
+{
+    string[] arr = str.Split(" ");
+    string result = String.Join(" ", arr.Reverse());
+    return result;
+}
+string revStr = ReversString(str);
+Console.WriteLine(revStr);
